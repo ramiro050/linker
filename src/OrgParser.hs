93e614c05504d7a2d2a4384dbb322fe05e6401e3
ramiro050 <ramiroleal050@gmail.com>
2020-08-10 14:01:39 -0500

    Added brackets to description generator

--- src/OrgParser.hs
+++ src/OrgParser.hs
@@ -7,39 +7,49 @@ module OrgParser
 import Text.Parsec
 import Text.Parsec.String
 
-data OrgLink = Link String String
+type Description = String
+type Link        = String
+
+data OrgLink = OrgLink Link Description
   deriving (Show, Eq)
 
+
 -- |Parsers the link part of an org-mode link, returning the link itself.
-orgLinkArg :: Parser String
+orgLinkArg :: Parser Link
 orgLinkArg = many $ (notFollowedBy (char ']')) >> anyChar
 
+
 -- |Parser the description part of an org-mode link, returning the description.
---
--- __Note:__ Org-mode allows square brackets in the description. The description
--- is only considered finished if it is followed by @]]@.
-orgDescArg :: Parser String
+-- |
+-- |__Note:__ Org-mode allows square brackets in the description. The description
+-- |is only considered finished if it is followed by @]]@.
+orgDescArg :: Parser Description
 orgDescArg = many $ (notFollowedBy (string "]]")) >> anyChar
+
 
 brackets :: Parser a -> Parser a
 brackets = between (char '[') (char ']')
 
+
 -- |Turn 'OrgLink' into a hyperlink that can be used in an org-mode file
 orgLinkToString :: OrgLink -> String
-orgLinkToString (Link l "") = "[[" ++ l ++ "]]"
-orgLinkToString (Link l d) = "[[" ++ l ++ "][" ++ d ++ "]]"
+orgLinkToString (OrgLink l "") = "[[" ++ l ++ "]]"
+orgLinkToString (OrgLink l d) = "[[" ++ l ++ "][" ++ d ++ "]]"
+
 
 -- |Parses hyperlinks used in org-mode files:
---
--- - @[[link]]@ or @[[link][description]]@
+-- |
+-- |- @[[link]]@ or @[[link][description]]@
 orgLink :: Parser OrgLink
-orgLink = brackets $ Link <$> link <*> desc
+orgLink = brackets $ OrgLink <$> link <*> desc
   where
     link = brackets orgLinkArg
     desc = brackets orgDescArg <|> string ""
 
+
 section :: Parser a -> Parser a
 section s = char '*' >> spaces *> s <* spaces
+
 
 nextSection :: Parser String
 nextSection = manyTill anyChar $ try (char '*')
--- test/OrgParserSpec.hs
+++ test/OrgParserSpec.hs
@@ -1,10 +1,12 @@
 module OrgParserSpec where
 
 import OrgParser
+import Data.List
 import Text.Parsec
 import Test.QuickCheck
 import Test.Hspec
 import Test.Hspec.QuickCheck
+
 
 -- |Generator for the link part of an org-mode link.
 linkGen :: Gen String
@@ -15,16 +17,22 @@ linkGen = resize 50 $ listOf (frequency [(15, letter), (10, num), (5, slash), (1
     slash = elements "/"
     space = elements " "
 
+
 -- |Generator for the description in an org-mode link.
---
--- __Note:__ This generator generates empty descriptions half of the time
--- in order to evenly generate links with and without descriptions.
+-- |
+-- |__Note:__ This generator generates empty descriptions half of the time
+-- |in order to evenly generate links with and without descriptions.
 descGen :: Gen String
-descGen = oneof [resize 30 $ listOf1 (elements "abcdABCD1234 "), pure ""]
+descGen = oneof [descString `suchThat` validRightBrackets, pure ""]
+  where
+    descString = resize 30 $ listOf1 (elements "abcdABCD1234 []")
+    validRightBrackets s = not $ ("]]" `isInfixOf` s || "]" `isSuffixOf` s)
+
 
 instance Arbitrary OrgLink where
   -- arbitrary :: Gen a
-  arbitrary = Link <$> linkGen <*> descGen
+  arbitrary = OrgLink <$> linkGen <*> descGen
+
 
 prop_parseLinks :: OrgLink -> Property
 prop_parseLinks l =
@@ -32,5 +40,7 @@ prop_parseLinks l =
     Left error -> property False
     Right l' -> l === l'
 
+
 spec :: Spec
-spec = prop "orgLink parser is the left inverse of orgLinkToString" prop_parseLinks
+spec = do
+  prop "orgLink parser is the left inverse of orgLinkToString" prop_parseLinks
